c0a8e7e66f57f829f7a94e23be57e54652f0cb24
budzik12 <81246476+budzik12@users.noreply.github.com>
2022-11-15 14:25:33 +0100

    create fun getLocation

--- app/src/main/java/com/example/camera/MainActivity.kt
+++ app/src/main/java/com/example/camera/MainActivity.kt
@@ -2,23 +2,41 @@ package com.example.camera
 
 import android.Manifest
 import android.app.DownloadManager.Request
+import android.content.ContentProviderClient
+import android.content.Context
 import android.content.Intent
 import android.content.pm.PackageManager
 import android.graphics.Bitmap
+import android.location.Location
+import android.location.LocationManager
+import android.location.LocationRequest
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.provider.MediaStore
+import android.util.Log
+import android.widget.Toast
 import androidx.core.app.ActivityCompat
 import com.example.camera.databinding.ActivityMainBinding
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationServices
 
 
 class MainActivity : AppCompatActivity() {
 
+    lateinit var fusedLocationProviderClient: FusedLocationProviderClient
+
+    lateinit var locationRequest: LocationRequest
+
     private var PERMISSION_ID = 52
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         val binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this)
+
         binding.button.isEnabled = false
         if (ActivityCompat.checkSelfPermission(this,Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED){
             ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.CAMERA), 123)
@@ -28,6 +46,16 @@ class MainActivity : AppCompatActivity() {
         binding.button.setOnClickListener {
             var i = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
             startActivityForResult(i,321)
+        }
+    }
+
+    private fun getLocation(){
+        if (CheckPermission()){
+            if(isLocationEnabled()){
+
+            }else{
+                Toast.makeText(this,"prosze pozwolic na korzystanie lokalizacji",Toast.LENGTH_SHORT).show()
+            }
         }
     }
 
@@ -54,7 +82,14 @@ class MainActivity : AppCompatActivity() {
             setContentView(binding.root)
             binding.button.isEnabled = true
         }
+
+        if(requestCode == PERMISSION_ID){
+            if(grantResults.isNotEmpty() && grantResults[0]==PackageManager.PERMISSION_GRANTED){
+                Log.d("Debug", "you have the permission")
+            }
+        }
     }
+
     //gps below
     private fun CheckPermission():Boolean{
         if(
@@ -72,6 +107,13 @@ class MainActivity : AppCompatActivity() {
             arrayOf(Manifest.permission.ACCESS_FINE_LOCATION,Manifest.permission.ACCESS_COARSE_LOCATION),PERMISSION_ID
         )
     }
+
+    private fun isLocationEnabled():Boolean{
+        var locationManager:LocationManager = getSystemService(Context.LOCATION_SERVICE) as LocationManager
+        return locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER) || locationManager.isProviderEnabled(LocationManager.NETWORK_PROVIDER)
+    }
+
+
 
 
 }
